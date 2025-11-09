486d538b2d9f2022554efc4b2991f78096f2e21a
Aidan-00 <maronnat.aidan84@gmail.com>
2025-11-09 16:11:42 +0100

    test

--- src/CMakeProject.cpp
+++ src/CMakeProject.cpp
@@ -1,20 +1,20 @@
 ﻿// CMakeProject.cpp : définit le point d'entrée de l'application.
 //
 
-//#include "CMakeProject.h"
-//#include "Source.cpp"
-//
-//using namespace std;
-//
-//int main()
-//{
-//	cout << "Hello CMake." << endl;
-//	Truc truc;
-//	cout << truc.JAJ();
-//	return 0;
-//}
+#include "CMakeProject.h"
+#include "Source.cpp"
 
-#include <catch2/catch_test_macros.hpp>
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	Truc truc;
+	cout << truc.JAJ();
+	return 0;
+}
+
+//#include <catch2/catch_test_macros.hpp>
 
 //TEST_CASE("Verifications sur un vector")
 //{
@@ -27,24 +27,24 @@
 //	CHECK(v[2] == 0);
 //}
 
-SCENARIO("Les vectors peuvent être dimensionnés et redimensionnés", "[vector]"){
-	GIVEN("Un vector avec quelques éléments"){
-		std::vector<int> v(5);
-		REQUIRE(v.size() == 5);
-
-		WHEN("La taille est augmentée") {
-			v.resize(10);
-			THEN("La taille et la capacité changent"){
-				REQUIRE(v.size() == 10);
-				REQUIRE(v.capacity() >= 10);
-			}
-		}
-		WHEN("Le vector est vidé") {
-			v.clear();
-			THEN("La taille est zéro mais la capacité est inchangée"){
-				REQUIRE(v.size() == 0);
-				REQUIRE(v.capacity() >= 5);
-			}
-		}
-	}
-}
+//SCENARIO("Les vectors peuvent être dimensionnés et redimensionnés", "[vector]"){
+//	GIVEN("Un vector avec quelques éléments"){
+//		std::vector<int> v(5);
+//		REQUIRE(v.size() == 5);
+//
+//		WHEN("La taille est augmentée") {
+//			v.resize(10);
+//			THEN("La taille et la capacité changent"){
+//				REQUIRE(v.size() == 10);
+//				REQUIRE(v.capacity() >= 10);
+//			}
+//		}
+//		WHEN("Le vector est vidé") {
+//			v.clear();
+//			THEN("La taille est zéro mais la capacité est inchangée"){
+//				REQUIRE(v.size() == 0);
+//				REQUIRE(v.capacity() >= 5);
+//			}
+//		}
+//	}
+//}
